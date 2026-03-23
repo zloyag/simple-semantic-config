f43b3db12f106c4bb39f19f635ebbb626ee3e14c
Alexandr Pichkur <alexandrpichkur@MacBook-Pro-Alexandr.local>
2026-03-23 15:06:07 +0200

    fix: EUI-2408 ONGOING - Dependabot Requests
    fixed out file

--- bin/semantic-release-pre.sh
+++ bin/semantic-release-pre.sh
@@ -11,7 +11,7 @@ mkdir $OUT_DIR
 if [[ "${BRANCH}" == "" ]]; then export BRANCH=${CIRCLE_BRANCH}; fi
 CIRCLE_PR_NUMBER="" CIRCLE_PR_REPONAME="" CIRCLE_PR_USERNAME="" CIRCLE_PULL_REQUEST="" CIRCLE_PULL_REQUESTS="" CI_PULL_REQUEST="" CI_PULL_REQUESTS="" yarn semantic-release --dry-run --branches $BRANCH > $OUT_DIR/semantic-dry.out || true
 cat $OUT_DIR/semantic-dry.out
-perl -ne 'print "$1\n" if /next release version is (.*)$/' $OUT_DIR/semantic-dry.out > $OUT_DIR/.version
+perl -ne 'print "$1\n" if /next release version is (.*)$/' $OUT_DIR/semantic-dry.out > $OUT_DIR/$OUT_FILE
 VERSION=$(cat $OUT_DIR/$OUT_FILE)
 if [ "${VERSION}" == "" ]; then
     echo "Semantic version isn't generated - please investigate"
